e2ab36f945efdbc7d121298b295c28c184d44609
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-09-27 15:39:50 +0000

    g

---
+++ phone shop assigment.py
@@ -0,0 +1,41 @@
+# funcions
+def GB64(x=0):
+ total+=200
+ return 0
+def GB128(x=0):
+ total+=350
+ return 0
+def dia(x=0):
+   total+=200
+   return 0
+def year1(x=0):
+   total+=20
+   input('please enter your email.')
+   return 0
+def year2(x=0):
+   total+=35
+   input('please enter your email')
+   return 0
+def protrbr(x=0):
+   total+=20
+   return 0
+def protcarb(x=0):
+   total+=100
+   return 0
+def charg(x=0):
+   total+=100
+   return 0
+esc=0
+while esc==0:
+    total=0
+    print('  welcome to phone shop.')
+    package=int(input ('''  what package would you like to buy?
+    1) McBasic Package
+    2) Average JoePackage  
+    3) Rich Kid ProPackage'''))
+    if package== 1 :
+       Choice=input('would you like an add on to your package?')
+       if Choice== 'y' or Choice== 'yes':
+         McBasic_options= input('''which one would you like?
+                 Rubber case
+                 Carbon Fibre case''')
