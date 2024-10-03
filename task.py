3023322953882abbd201d9fe32f592298cffda49
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-10-03 15:39:45 +0000

    g

--- task.py
+++ task.py
@@ -1,5 +1,17 @@
 import math
-def cirle(r,):
+def circleD(r):
+ D=2*r
+ return D
+def cylinderarea(r,h=float(10)):
+ A=2*math.pi*r*(r+h)
+ return A
+def conearea(r,h=10):
+ A=(math.pi*r*r)+(math.pi*r*math.sqrt(r**2+h**2))
+ return A
+def spherearea(r,h=10):
+ A=4*math.pi*r*r
+ return A
+def circle(r,):
  A=math.pi*r*r
  return A
 def cylinder(r,h=10):
@@ -14,7 +26,11 @@ def sphere(r,h=10):
  V=(math.pi*r**3)*4/3
  return V
 radius=float(input('what is the radius?'))
-print ('area of the cirlce is',cirle(radius),'cm')
-print (' volume of the cylinder is',cylinder(radius),'cm')
-print ('volume of the cone is',cone(radius),'cm')
-print ('volume of the sphere is',sphere(radius),'cm')
+print ('area of the cirlce is',circle(radius),'cm squared')
+print (' volume of the cylinder is',cylinder(radius),'cubic cm')
+print ('volume of the cone is',cone(radius),'cubic cm')
+print ('volume of the sphere is',sphere(radius),'cubic cm')
+print ('the diameter of the circle is',circleD(radius),'cm')
+print ('the surface area of the cylinder is', cylinderarea(radius,'cm squared'))
+print ('the surface area of the sphere  is', spherearea(radius,'cm squared'))
+print ('the surface area of the cone is', conearea(radius,'cm squared'))
