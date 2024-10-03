9e0d4c71f9c6a08f01053cf831dee722174a72a4
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-10-03 14:18:33 +0000

    ert

--- phoneshopbutbettter.py
+++ phoneshopbutbettter.py
@@ -7,17 +7,11 @@ def GBchoice(x=0):
  if ask==1:
      return(0)
  elif ask==2:
-   x=GB64()
+   x+=200
    return(x)
  elif ask==3:
-   x=GB128()
+   x=+350
    return(x)
-def GB64(x=0):
- x+=200
- return x
-def GB128(x=0):
- x+=350
- return x
 def dia(x=0):
 
    ask=input('would you like extra diamonds?')
@@ -34,25 +28,11 @@ def yearchoice(x=0):
   if ask==1:
     return(0)
   elif ask==2:
-    x=year1()
+    x+=20
     return x
   elif ask==3:
-    x=year2()
+    x+=35
     return x
-def year1(x=0):
-   x+=20
-   input('please enter your email:')
-   return x
-def year2(x=0):
-   x+=35
-   input('please enter your email:')
-   return x
-def protrbr(x=0):
-   x+=20
-   return x
-def protcarb(x=0):
-   x+=100
-   return x
 def charg(x=0):
    ask=input('would you like a extra fast battery charger?')
    if ask=='n' or ask=='no':
@@ -66,10 +46,10 @@ def protchoice(x=0):
    2) Rubber $20
    3) Crbonfibre $100'''))
  if protchoice== 2:
-    x=protrbr()
+    x+=20
     return x
  elif protchoice == 3:
-    x=protcarb()
+    x+=100
     return x
  elif protchoice:
     return (0)
@@ -87,7 +67,7 @@ try:
       print('  welcome to phone shop.')
       package=int(input ('''  what package would you like to buy?
       1) McBasic Package
-      2) Average JoePackage  
+      2) Average JoePackage
       3) Rich Kid ProPackage'''))
       if package==1:
          total+=50
