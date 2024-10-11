987e0a9593babd36fa3710b77e6a2a052fd15149
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-10-11 15:36:53 +0000

    98

---
+++ MadLibs.py
@@ -0,0 +1,29 @@
+adj1=input('any adjective')
+adj2=input('any adjective')
+adj3=input('any adjective')
+number=input('any date')
+name1=input('a word you would call someone')
+adv1=input('an adverb')
+food=input('anything edible')
+name2=input('something people like')
+name3=input('something short')
+name4=input('something valuable')
+pluralnoun2=input('some group of people')
+pluralnoun1=input('plural noun')
+pluralnoun3=input('something useless')
+name5=input('important person')
+year=input('a year')
+print(f'''
+ now is your chance to enter this {adj1} contest. Anyone,
+ and we mean anyone, can enter this {adj2} contest. Just
+ follow these {adj3} rules:
+ Write down in {number}.words or less why you think that
+ person you know.should be elected "{name1}.of the Year.
+ Remember, he/she does not know that you think so adverb
+ of him/her. First prize will be a deluxe three-speed {name2},
+ plus a year's supply of {food}. Second prize is a 21-foot
+ {name3}. Third prize is a full-color {name4}, plus a set
+ of {pluralnoun1}. Each entry must be accompanied by a stamped,
+ self-addressed {name5}. Decision of the {pluralnoun2}
+ will be announced in {year}.and will be final. In the event of a
+ tie, duplicate {pluralnoun3}.will be awarded.''')
