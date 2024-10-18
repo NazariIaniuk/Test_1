64ce1527de2ebb62a6794b7e5d3927c28e30b29d
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-10-18 15:39:33 +0000

    d

--- container5.py
+++ container5.py
@@ -1,1 +1,4 @@
-freddie=['Killer qeen']
+freddie=['Killer qeen','Another one bites the dust','Dont stop me now']
+Michael=['Billie Jean','Blood on the dance floor','beat it']
+Christopher=['Greenpath','Mantis Lords','Path of pain']
+dictoonary={'Freddie Mercury':freddie, 'Michael Jackson':Michael,'Christopher Larkin':Christopher}
