91a0e72768f1c767bc8bcbbbe9e6a7b1113f31a2
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-10-21 14:30:25 +0000

    f

--- challanges/container challanges/container5.py
+++ challanges/container challanges/container5.py
@@ -1,4 +1,5 @@
 freddie=['Killer qeen','Another one bites the dust','Dont stop me now']
 Michael=['Billie Jean','Blood on the dance floor','beat it']
 Christopher=['Greenpath','Mantis Lords','Path of pain']
-dictoonary={'Freddie Mercury':freddie, 'Michael Jackson':Michael,'Christopher Larkin':Christopher}
+dictionary={'Freddie Mercury':freddie, 'Michael Jackson':Michael,'Christopher Larkin':Christopher}
+print(dictionary)
