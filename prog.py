b02991276123230c3c67f04e007ac868e1394876
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-11-05 15:50:24 +0000

    g

---
+++ prog.py
@@ -0,0 +1,4 @@
+grades = {"Susan":{"Math":45,"Enlish":37,"Fr":54},
+            "Mark":{"Math":62,"Enlish":58,"French":59},
+            "Hohn":{"Math":49,"Enlish":47,"French":60},
+            "Andy":{"Math":78,"Enlish":83,"French":62}}
