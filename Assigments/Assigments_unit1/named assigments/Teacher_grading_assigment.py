8f9a1b69ffbf7d9f31e4598c596f4bb457c729d8
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-11-14 19:28:17 +0000

    z

---
+++ Assigments/Assigments_unit1/named assigments/Teacher_grading_assigment.py
@@ -0,0 +1,29 @@
+import statistics
+grades=[]
+minidict=dict()
+while True:
+    try:
+            ask1=int(input('how many students are there?'))
+            ask2=int(input('how many asigments are there?'))
+            for i in range(0,ask1):
+                name=input('what is the name of the student?')
+                minilist=[]
+                for i in range(0,ask2):
+                 value=int(input('what is the total value of the assignment?'))
+                 grade=int(input('what is the grade the student got?'))
+                 minilist.append(grade/value*100)
+                 grades.append(grade/value*100)
+                minidict[name]=minilist
+            print(minidict)
+            break
+    except ValueError or ZeroDivisionError:
+          print('please input a proper number when it is asked to do so.')
+print(f'the mean of class grades,{statistics.mean(grades)}%')
+print(f'the median of class grades,{statistics.median(grades)}%')
+print(f'the mode of class grades,{statistics.mode(grades)}%')
+while True:
+ ask=input('would like to check the grade(s) of a specific student?')
+ if ask=='y' or ask=='yes':
+  print(minidict[input('whoose grade(s) would you like to see?')])
+ if ask=='n' or ask=='no':
+    break
--- prog.py
+++
@@ -1,7 +0,0 @@
-grades = {"Susan":{"Math":45,"Enlish":37,"Fr":54},
-            "Mark":{"Math":62,"Enlish":58,"French":59},
-            "Hohn":{"Math":49,"Enlish":47,"French":60},
-            "Andy":{"Math":78,"Enlish":83,"French":62}}
-print(grades ["Mark"]["French"])
-grades["Mark"]["French"]=67
-print(grades ["Mark"]["French"])
