294e319ec0fb94c82078b8cc84aa207f76c505cf
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-11-25 16:09:10 +0000

    s

--- challanges/Unit 2/textchallanges/textfilechallange6.py
+++ challanges/Unit 2/textchallanges/textfilechallange6.py
@@ -1,4 +1,15 @@
-namefile= open('Names.txt', 'r')
+namefile=open('Names.txt', 'r')
+Namefile2=open('Names2.txt','w')
 print(namefile.read())
-namelist=namefile.read()
-print(namelist)
+find=input('enter a name  that is in the list ')
+for row in 'Names.txt' :
+    if find in 'Names.txt':
+     print('deleted')
+    else:
+        Namefile2=open('Names2.txt','')
+        Namefile2.write(row)
+Namefile2=open('Names2.txt','r')
+print(Namefile2.read)
+
+
+
