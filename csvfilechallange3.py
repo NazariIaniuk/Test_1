d73b5f342c15ed4e9b24c6da07428b837ea53228
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2024-11-25 15:30:27 +0000

    s

--- csvfilechallange3.py
+++ csvfilechallange3.py
@@ -1,9 +1,14 @@
 import csv
-ask=int(input('how many files do you want to add?'))
-for i in range(0,ask):
-    books=open("books.csv", "a")
+for i in range(0,int(input('how many files do you want to add?'))):
+    books=open('books.csv', 'a')
     book=input('Enter name of the book:  ')   
     Author=input('Enter the name of the authror:   ')
     year=input('Enter the release date of the book:  ')
     bookinfo='\n'+book+','+Author+','+year
     books.write(str(bookinfo))
+books=open ('books.csv', 'r')
+reader=csv.reader(books)
+searchrequest=input('enter the name of the author you want to find:  ')
+for row in books:
+    if searchrequest in str(row):
+        print (row)
