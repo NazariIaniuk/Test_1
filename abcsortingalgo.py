0d5d9912ae7e89eab226b843d280e6841690c8e9
NazariIaniuk <nianiuk1008@bhncdsb.ca>
2025-02-07 19:06:22 +0000

    9

--- abcsortingalgo.py
+++
@@ -1,7 +0,0 @@
-
-msglist=['b','a','c']
-msglistsort=[]
-msglist.sort()
-print(msglist)
-
-dictionary={5:3}
--- acccounting.py
+++ acccounting.py
@@ -1,33 +1,16 @@
 def balancesheet(x=0):
- ask1=int(input('''what do you have availiable to you?
-                1 assets and equity
-                2 liabilities and equity
-                3 assets and liabilities
-                4 all information'''))
-def assetscalc(x=0):
- dictionarylist=[]
- sortinglist=[]
+ assetlist=[]
+ valuelist=[]
  ask1='y'
  while ask1!='no':
   asset=input('name of asset:')
   number=int(input('price tied to asset'))
-  dictionary={number:asset}
-  sortinglist.append(number)
-  dictionarylist.append(dictionary)
-  ask1=input('add more?')
- sortinglist.sort()
- print(sortinglist)
- print(dictionarylist)
+  assetlist.append(asset)
+  valuelist.append(number)
 
-f
 
   
   
-
-
-
-
-
 
 
 
